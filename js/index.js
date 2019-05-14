f91082dd5298fcfad72454efa6f7aa91b9df2632
Elise Spain <elisespain@gmail.com>
2019-05-14 15:25:09 -0700

    dbl click  works finally

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+// Double click 
+const buttonUpdate = document.querySelector('.destination');
+
+buttonUpdate.addEventListener('dblclick',event => {
+  event.target.style.fontSize= '50px';
+});
+
+//scroll
+
+//mouseover
+const mouseSpice = document.querySelector('.destination');
+mouseSpice.addEventListener('mouseover',event => {
+    event.target.style.color = 'green';
+});
+
+//keydown
+const keysPlease = document.querySelectorAll('.content-destination img');
+keysPlease.addEventListener("keydown", event =>{
+
+})
+//wheel
+
+//drag/drop
+
+//load
+//focus
+//resize
+//select
