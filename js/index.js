5e92ea5b81017322f63f44577a351683a63f5200
Elise Spain <elisespain@gmail.com>
2019-05-14 15:46:35 -0700

    mouse over looks better

--- js/index.js
+++ js/index.js
@@ -8,22 +8,23 @@ buttonUpdate.addEventListener('dblclick',event => {
 });
 
 //scroll
-
+const scrollUpdate = document.querySelector('')
 //mouseover
-const mouseSpice = document.querySelector('.destination');
+const mouseSpice = document.querySelector('.intro h2');
 mouseSpice.addEventListener('mouseover',event => {
-    event.target.style.color = 'green';
+    event.target.style.color = '#42f4d7';
+    event.target.style.fontSize = '45px';
 });
 
 //keydown
-const keysPlease = document.querySelectorAll('.content-destination img');
+const keysPlease = document.querySelector('.content-destination img');
 keysPlease.addEventListener("keydown", event =>{
 
 })
 //wheel
 
 //drag/drop
-
+const dragOn = document.querySelector(nsmnd)
 //load
 //focus
 //resize
